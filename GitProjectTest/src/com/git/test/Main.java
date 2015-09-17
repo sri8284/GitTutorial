00fb7be0e21ee2e084e45ead5ba3c531938e2af5
savya <savya@srikanth>
2015-09-17 20:35:18 +0530

    commited 3

--- GitProjectTest/src/com/git/test/Main.java
+++ GitProjectTest/src/com/git/test/Main.java
@@ -3,7 +3,8 @@ package com.git.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Testing git");
+		System.out.println("Testing git--------changed");
+		System.out.println("done");
 	}
 	
 	private void getHi(){
