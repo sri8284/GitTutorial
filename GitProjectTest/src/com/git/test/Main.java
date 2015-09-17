0f2d2c178c3842c0ee28ff4f9423b97644c6811e
savya <savya@srikanth>
2015-09-17 22:48:46 +0530

    commited 7

--- GitProjectTest/src/com/git/test/Main.java
+++ GitProjectTest/src/com/git/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Testing git--------changed");
 		System.out.println("doned");
+		System.out.println("commited 7");
 	}
 	
 	private void getHi(){
